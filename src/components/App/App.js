281bdd77a6bee43cccb915ff16a1eafb417324e9
malcar <malcar@malcars-MacBook.local>
2020-07-16 11:29:02 -0400

    app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,8 +8,8 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
-// import IndexProducts from '../Products/IndexProducts'
-import Products from '../Products/products'
+import IndexProducts from '../Products/IndexProducts'
+// import Products from '../Products/products'
 
 class App extends Component {
   constructor () {
@@ -56,7 +56,7 @@ class App extends Component {
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword msgAlert={this.msgAlert} user={user} />
           )} />
-          <Route path="/products" component={Products}/>
+          <Route path="/products" component={IndexProducts}/>
         </main>
       </Fragment>
     )
