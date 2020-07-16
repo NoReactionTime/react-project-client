b90f3970b9de8b7f4a59177d99010c39f62b8ec3
kkorrapaty <kkorrapatyjr@gmail.com>
2020-07-16 16:02:18 -0400

    Cart
    Routes work and products can be added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import axios from 'axios'
-// import Products from '../Product/IndexProducts'
-import apiUrl from '../../apiConfig'
+import Container from 'react-bootstrap/Container'
+import { Row, Col } from 'react-bootstrap'
+
+// import axios from 'axios'
+// // import Products from '../Product/IndexProducts'
+// import apiUrl from '../../apiConfig'
 
 const save = require('../../save.js')
 
@@ -11,52 +14,60 @@ class Cart extends Component {
     super()
 
     this.state = {
-      product: null,
-      quantity: null,
-      purchased: null
+      orderItems: {
+        product: null,
+        quantity: null,
+        purchased: null
+      }
     }
   }
 
-  componentDidMount () {
-    axios.get(`${apiUrl}/orderitems`)
-      // .then(res => console.log(res))
-      .then(response => {
-        // handle success
-        this.setState({
-          product: save.cart.items
-        })
-      })
-      .catch(error => {
-        // handle error
-        console.log(error)
-      })
-  }
+  // componentDidMount () {
+  //   console.log(save.cart)
+  //   axios.get(`${apiUrl}/orderitems`)
+  //   // .then(res => console.log(res))
+  //     .then(() => console.log(save.cart))
+  //     .then(response => {
+  //       // handle success
+  //       this.setState({
+  //         orderItems: save.cart.items[0]
+  //       })
+  //     })
+  //     .catch(error => {
+  //       // handle error
+  //       console.log(error)
+  //     })
+  // }
 
   render () {
     let jsx
     // if the API has not responded yet
-    if (this.state.product === null) {
+    if (save.cart.items[0] === null) {
       jsx = <p>Loading...</p>
     // if the API responds with no books
-    } else if (this.state.quantity === 0) {
+    } else if (save.cart.items === 0) {
       jsx = <p>No products</p>
     // if the API responds with books
     } else {
       jsx = (
-        <ul>
-          {this.state.products.map(product => {
-            return (
-              <li key={product._id}>
-                <Link to={`/orderitems/${product._id}`}>{product.name}</Link>
-              </li>
-            )
-          })}
-        </ul>
+        <Container>
+          <Row>
+            {save.cart.items.map(product => {
+              return (
+                <Col key={product._id}>
+                  <Link to={`/products/${product._id}`}><h3>{product.name}</h3></Link>
+                  <h4>Description: {product.description}</h4>
+                  <h4>Price: $ {product.unitPrice}</h4>
+                </Col>
+              )
+            })}
+          </Row>
+        </Container>
       )
     }
     return (
       <div>
-        <h2>Product Page</h2>
+        <h2>Shopping Cart</h2>
         {jsx}
       </div>
     )
--- src/components/Products/ShowProduct.js
+++ src/components/Products/ShowProduct.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
-
 import Button from 'react-bootstrap/Button'
 const save = require('../../save.js')
 
+<<<<<<< HEAD
 class ShowProduct extends React.Component {
   state = {
     product: null
@@ -12,24 +12,46 @@ class ShowProduct extends React.Component {
 
   componentDidMount () {
     axios.get(`${apiUrl}/products/` + this.props.match.params.id)
+=======
+const ShowProduct = props => {
+  const [product, setProduct] = useState(null)
+  // const [add, setAdd] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${apiUrl}/products/` + props.match.params.id)
+>>>>>>> Cart
       .then(response => {
       // handle success
-        this.setState({
-          product: response.data.product
-        })
+        setProduct(response.data.product)
       })
       .catch(error => {
         // handle error
         console.log(error)
       })
-  }
+  }, [])
 
-  saveData (product) {
+  // useEffect(() => {
+  //   if (add === 0) {
+  //     if (save.cart.items.indexOf(product) === -1) {
+  //       save.cart.items.push(product)
+  //     }
+  //     console.log(product)
+  //     console.log(save)
+  //   }
+  // }, [])
+
+  function saveData (product) {
+    if (save.cart.items.indexOf(product) === -1) {
+      return save.cart.items.push(product)
+    }
     console.log(product)
     console.log(save)
-    save.cart.items.push(product)
+    // useEffect((product) => {
+    //
+    // }, [])
   }
 
+<<<<<<< HEAD
   render () {
     const { product } = this.state
     let jsx
@@ -49,12 +71,31 @@ class ShowProduct extends React.Component {
       )
     }
     return (
+=======
+  let jsx
+  // if the API has not responded yet
+  if (product === null) {
+    jsx = <p>Loading...</p>
+  // after API responds
+  } else {
+    jsx = (
+>>>>>>> Cart
       <div>
-        <h2>Single Product Page</h2>
-        {jsx}
+        <h3>{product.name}</h3>
+        <h4>Description: {product.description}</h4>
+        <h4>Price: ${product.unitPrice}</h4>
+        <Button variant="primary" onClick={() => {
+          saveData(product)
+        }}>Add To Cart</Button>
       </div>
     )
   }
+  return (
+    <div>
+      <h2>Single Product Page</h2>
+      {jsx}
+    </div>
+  )
 }
 
 export default ShowProduct
